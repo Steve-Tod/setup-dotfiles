74b8a2204543e75f459b3d970ac21993915b3769
Tod <785590489@qq.com>
2018-09-28 07:39:37 +0000

    fix bug, remove .zshrc

--- zsh_init.sh
+++ zsh_init.sh
@@ -1,5 +1,5 @@
 # oh-my-zsh
-sudo apt-get zsh
+sudo apt-get install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 zsh
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
