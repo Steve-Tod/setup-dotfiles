f9fc105cbec182881ca2d775c92aa4bbdc1f7ec3
Zhenyu Jiang <zhenyu@cs.utexas.edu>
2020-07-19 23:02:37 -0500

    update

--- scripts/ycm_init.sh
+++ scripts/ycm_init.sh
@@ -1,10 +1,9 @@
 # ycm
 # need build-essential cmake python-dev python3-dev
 # python -c "from utils import search_package_and_install; search_package_and_install(['build-essential', 'cmake', 'python-dev', 'python3-dev'])"
-sed -E 's/" Plugin(.*YouCompleteMe.*$)/Plugin\1/g' ~/.vimrc > ~/.vimrc
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 ./install.py --clang-completer
-cp ../dotfiles/.ycm_extra_conf.py ~/.vim/
 cd -
+cp ../dotfiles/.ycm_extra_conf.py ~/.vim/
 cat ycm_vim_conf_str.txt >> ~/.vimrc
--- scripts/zsh_init.sh
+++ scripts/zsh_init.sh
@@ -1,4 +1,3 @@
 # oh-my-zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cp ../dotfiles/.zshrc ~/.zshrc
-zsh
