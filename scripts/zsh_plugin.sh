b9a66c4d7ae5959187bdc50245ad7f8d99bb7d1a
Zhenyu Jiang <stevetod@163.com>
2021-09-25 18:00:44 -0500

    Update zsh_plugin.sh

--- scripts/zsh_plugin.sh
+++ scripts/zsh_plugin.sh
@@ -1,4 +1,4 @@
-git clone git@github.com:zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone git@github.com:zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com:zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 python -c "from utils import modify_zshrc; modify_zshrc(['git', 'zsh-autosuggestions', 'zsh-syntax-highlighting'])"
 source ~/.zshrc
