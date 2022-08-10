b5447dfc2981169007d45e49665dee6e60f1d9c0
Zhenyu Jiang <zhejiang@nvidia.com>
2022-08-10 11:16:30 -0700

    fix tmux conf

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,7 +1,1 @@
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-
-cd -
-cat tmux_navigator_str.txt >> ~/.tmux.conf
+cp ../dotfiles/.tmux.* ~/
